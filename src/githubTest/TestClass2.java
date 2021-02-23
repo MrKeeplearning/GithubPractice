a0f116f435ce9b107fb6e73979fbcbedbb65b85f
jaeseok <jaeseok1115@gmail.com>
2021-02-23 23:20:11 +0900

    This is second commit message.

---
+++ src/githubTest/TestClass2.java
@@ -0,0 +1,17 @@
+package githubTest;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Supra?");
+		System.out.println("Holy god is that a supra in the bay?!");
+		System.out.println("Is..is.. that a soupra??");
+		System.out.println("Wooow!");
+		System.out.println("(washing out his glasses) IS THAT A SUPRA!!!?");
+		System.out.println("DUDE IS THAT A SUPRA!!!");
+		System.out.println("DUDE IS THAT A SUPRA???");
+		System.out.println("DUDE IS THAT A FREAKING SUPRA???");
+		System.out.println("yes it's a supra...");		
+	}
+
+}
